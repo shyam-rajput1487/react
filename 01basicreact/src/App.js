a91497c9fabb7c349fccf02c5f0567b7f0941cbf
shyam-rajput1487 <shyammohan.off@gmail.com>
2024-09-25 11:37:45 +0000

    basic understanding

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Kull with react</h1>
+  );
+}
+
+export default App;
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
-import Chai from "./chai"
+
+
 
 function App() {
+  
+
   return (
-
-    <Chai />
-
+   <h1>Kull Technogy with vite </h1>
   )
 }
 
