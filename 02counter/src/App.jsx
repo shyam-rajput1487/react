f6c7fe5c9bc48a5483b6fa6efdec643c6adab511
shyam-rajput1487 <shyammohan.off@gmail.com>
2024-10-09 08:58:58 +0000

    add small pro

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCount] = useState(0)
+  const add=()=>{
+    if(counter<20){
+      setCount(counter+1)
+    }
+  }
+
+  const remove=()=>{
+    if (counter>0) {
+      setCount(counter-1)
+    }
+   
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+      <h1>Youtube Fun with react</h1>
+     <h2>Counter value: {counter}</h2>
+ 
+     <button onClick={add}>Add value</button>
+     <button onClick={remove}>Remove value</button>
+      
+    
     </>
   )
 }
